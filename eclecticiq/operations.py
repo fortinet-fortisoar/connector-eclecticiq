4c3ccaac2969f8ef57bd1a32fb06ede40a516456
prerna Joshi <prerna@cybersponse.com>
2021-05-27 15:32:25 +0530

    Fix against bug #0717745

--- eclecticiq/operations.py
+++ eclecticiq/operations.py
@@ -18,7 +18,8 @@ def get_config_data(config):
 def eiq_init(config):
     eiq_url, eiq_user, eiq_password, verify_ssl = get_config_data(config)
     try:
-        eiq = eiq_api.EclecticIQ_api(baseurl=eiq_url, eiq_version="2.7", username=eiq_user, password=eiq_password, verify_ssl=verify_ssl)
+        eiq = eiq_api.EclecticIQ_api(baseurl=eiq_url, eiq_version="2.7", username=eiq_user, password=eiq_password,
+                                     verify_ssl=verify_ssl)
         return eiq
     except Exception as e:
         logger.exception("Exception to connect: {}".format(e))
@@ -27,10 +28,10 @@ def eiq_init(config):
 
 def get_observable_reputation(config, operation_name, params):
     observable_type_dict = {'get_ip_reputation': 'ipv4',
-        'get_domain_reputation': 'domain',
-        'get_email_reputation': 'email',
-        'get_file_reputation': 'file,hash-md5,hash-sha1,hash-sha256,hash-sha512',
-        'get_uri_reputation': 'uri'} 
+                            'get_domain_reputation': 'domain',
+                            'get_email_reputation': 'email',
+                            'get_file_reputation': 'file,hash-md5,hash-sha1,hash-sha256,hash-sha512',
+                            'get_uri_reputation': 'uri'}
 
     observable_value = params.get('observable')
     observable_type = observable_type_dict.get(str(operation_name))
@@ -61,25 +62,25 @@ def get_observable_reputation(config, operation_name, params):
 
 def prepare_observables(param):
     valid_ops = {
-                        "Domain":"domain",
-                        "Email":"email",
-                        "Email Subject":"email-subject",
-                        "File":"file",
-                        "Hash":"hash",
-                        "Hash MD5":"hash-md5",
-                        "Hash SHA1":"hash-sha1",
-                        "Hash SHA256":"hash-sha256",
-                        "Hash SHA512":"hash-sha512",
-                        "Host":"host",
-                        "IPv4":"ipv4",
-                        "IPv6":"ipv6",
-                        "Mutex":"mutex",
-                        "Port":"port",
-                        "Process":"process",
-                        "URI":"uri",
-                        "URI Hash SHA256":"uri-hash-sha256",
-                        "Win Registry":"winregistry"
-                        }
+        "Domain": "domain",
+        "Email": "email",
+        "Email Subject": "email-subject",
+        "File": "file",
+        "Hash": "hash",
+        "Hash MD5": "hash-md5",
+        "Hash SHA1": "hash-sha1",
+        "Hash SHA256": "hash-sha256",
+        "Hash SHA512": "hash-sha512",
+        "Host": "host",
+        "IPv4": "ipv4",
+        "IPv6": "ipv6",
+        "Mutex": "mutex",
+        "Port": "port",
+        "Process": "process",
+        "URI": "uri",
+        "URI Hash SHA256": "uri-hash-sha256",
+        "Win Registry": "winregistry"
+    }
 
     observable_params = [
         (
@@ -127,7 +128,8 @@ def create_sighting(config, operation_name, params):
     eiq_group = config.get('eiq_group', None)
 
     observables_dict = prepare_observables(params)
-    sighting_conf_value = "None" if params.get('confidence_value') == "No Confidence(None)" else params.get('confidence_value')
+    sighting_conf_value = "None" if params.get('confidence_value') == "No Confidence(None)" else params.get(
+        'confidence_value')
     sighting_title = params.get('sighting_title')
     sighting_tags = params.get('tags').split(",")
     sighting_impact_value = "None" if params.get('impact_value') == "No Impact(None)" else params.get('impact_value')
@@ -136,9 +138,9 @@ def create_sighting(config, operation_name, params):
     try:
         eiq = eiq_init(config)
         sighting = eiq.create_entity(observable_dict=observables_dict, source_group_name=eiq_group,
-                                              entity_title=sighting_title, entity_description=sighting_description,
-                                              entity_tags=sighting_tags, entity_confidence=sighting_conf_value,
-                                              entity_impact_value=sighting_impact_value)
+                                     entity_title=sighting_title, entity_description=sighting_description,
+                                     entity_tags=sighting_tags, entity_confidence=sighting_conf_value,
+                                     entity_impact_value=sighting_impact_value)
 
         if sighting is not False:
             return {"result": sighting, "status": "success"}
@@ -151,27 +153,34 @@ def create_sighting(config, operation_name, params):
 
 
 def query_entities(config, operation_name, params):
-    if params['entity_type'] == "all":
-        entity_type = '("campaign" OR "course-of-action" OR "exploit-target" OR "incident" OR' \
-                      ' "indicator" OR "threat-actor" OR "ttp")'
+    entity_type_dict = {"All": "all", "Campaign": "campaign",
+                        "Course of Action": "course-of-action",
+                        "Exploit Target": "exploit-target",
+                        "Incident": "incident", "Indicator": "indicator",
+                        "Threat Actor": "threat-actor", "TTP": "ttp"}
+    if params['entity_type'] == "All":
+        entity_type = '("ttp" OR "campaign" OR "course-of-action" OR "exploit-target" OR "incident" OR' \
+                      ' "indicator" OR "threat-actor")'
     else:
-        entity_type = params['entity_type']
-
+        entity_type = entity_type_dict.get(params['entity_type'])
     if params["query"] == "":
         query = None
     else:
         query = params["query"]
-
     if params["entity_value"] == "":
         entity_value = None
     else:
         entity_value = params["entity_value"]
 
+    query_params = {
+        "from": params.get("from", 0),
+        "size": params.get("size", 10)
+    }
     try:
         eiq = eiq_init(config)
-        query_result = eiq.search_entity(entity_value=entity_value, entity_type=entity_type, observable_value=query)
+        query_result, result_size = eiq.search_entity(entity_value=entity_value, entity_type=entity_type,
+                                                      observable_value=query, params=query_params)
         result = []
-
         if query_result is not False:
             for k in query_result:
                 if len(k['_source']['extracts']) > 0:
@@ -222,11 +231,16 @@ def query_entities(config, operation_name, params):
                         'source_name': response_source_name
                     }
                     result.append(parsed_response)
-
-            return {"result": result, "status": "success"}
+            return {"status": "success",
+                    "actual_result_size": result_size["value"],
+                    "reported_results": len(result),
+                    "result": result
+                    }
         else:
-            return {"result": [], "status": "success"}
-
+            return {"result": [], "status": "success",
+                    "actual_result_size": result_size["value"],
+                    "reported_results": len(result)
+                    }
     except Exception as e:
         logger.exception("Error: {0}".format(e))
         raise ConnectorError("Error: {0}".format(e))
